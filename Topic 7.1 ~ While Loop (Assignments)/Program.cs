5f1f33a2ead7187fd0d14e7d23f49bb08997ce10
Moonismylover <Angelpreetthind9@gmail.com>
2026-03-18 14:32:52 -0400

    Add project files.

---
+++ Topic 7.1 ~ While Loop (Assignments)/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Topic_7._1___While_Loop__Assignments_
+{
+    internal class Program
+    {
+        public static void Assignment1()
+        {
+            Console.Title = "Average of Numbers in a Loop";
+
+            int numInputs, counter = 0;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Hello, user! How many numbers do you like to input? \nEnter here: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            numInputs = Convert.ToInt32(Console.ReadLine());
+
+            //while (counter <= numInputs)
+            //{
+                
+            //}
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Topic 7.1 ~ While Loop (Assignments)";
+
+            Assignment1();
+
+               
+        }
+    }
+}
