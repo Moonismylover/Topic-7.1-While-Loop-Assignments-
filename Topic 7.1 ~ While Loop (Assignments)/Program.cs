87cbc44eefbdd68550d23597ca9367e229c2a958
Moonismylover <Angelpreetthind9@gmail.com>
2026-03-18 20:28:33 -0400

    yayyy

--- Topic 7.1 ~ While Loop (Assignments)/Program.cs
+++ Topic 7.1 ~ While Loop (Assignments)/Program.cs
@@ -6,26 +6,51 @@
         {
             Console.Title = "Average of Numbers in a Loop";
 
-            int numInputs, counter = 0, sum = 0, num, final;
+            int numInputs, counter = 0;
+            double num, sum = 0, final;
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("Hello, user! How many numbers do you like to input? \nEnter here: ");
+            Console.Write("Hello, user! How many numbers would you like to input? \nEnter here: ");
             Console.ForegroundColor = ConsoleColor.Blue;
             numInputs = Convert.ToInt32(Console.ReadLine());
 
-            while (counter <= numInputs)
+            while (numInputs <= 0)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Number: ");
+                Console.WriteLine("Invalid input. Try again!");
+                Console.Write("How many numbers would you like to input? \nEnter here: ");
                 Console.ForegroundColor = ConsoleColor.Blue;
-                num = Convert.ToInt32(Console.ReadLine());
+                numInputs = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            while (counter < numInputs)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Number: ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Double.TryParse(Console.ReadLine(), out num);
                 sum = sum + num;
                 counter++;
             }
 
             Console.ForegroundColor = ConsoleColor.Yellow;
             final = sum / numInputs;
-            Console.WriteLine($"The average of your numbers is {final}!");
+            Console.WriteLine($"The average of your numbers is {final.ToString("F2")}!");
+        }
+
+        public static void Assignment2()
+        {
+            Console.Title = "Hi Low with Limited Tries";
+
+            int num;
+
+            Random generator = new Random();
+            num = generator.Next(1, 101);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Welcome USER! You will be given 7 guesses to guess a randomly generated number from 1-100. GOOD LUCK!");
+
+
         }
 
         static void Main(string[] args)
